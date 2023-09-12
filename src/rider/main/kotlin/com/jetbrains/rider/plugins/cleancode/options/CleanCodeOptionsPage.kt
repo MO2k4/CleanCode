0d23dc3569d0a27cd83b004223d5c707a514ba83
Martin Oehlert <mo1987@gmx.net>
2023-09-12 12:29:29 +0200

    fix issue #184

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/cleancode/options/CleanCodeOptionsPage.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/cleancode/options/CleanCodeOptionsPage.kt
@@ -2,7 +2,9 @@ package com.jetbrains.rider.plugins.cleancode.options
 
 import com.jetbrains.rider.settings.simple.SimpleOptionsPage
 
-class CleanCodeOptionsPage : SimpleOptionsPage("CleanCode", "CleanCodeAnalysisOptionsPage") {
+class CleanCodeOptionsPage : SimpleOptionsPage(
+        name= CleanCodeBundle.message("configurable.name.cleancode.options.title"),
+        pageId = "CleanCodeAnalysisOptionsPage") {
     override fun getId(): String {
         return "CleanCodeAnalysisOptionsPage";
     }
