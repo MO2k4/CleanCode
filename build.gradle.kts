85f031d25ed8410ccbfe0095a585c29040fc7c6c
Martin Oehlert <mo1987@gmx.net>
2024-08-21 10:25:05 +0200

    tryout

--- build.gradle.kts
+++ build.gradle.kts
@@ -201,12 +201,12 @@ tasks.prepareSandbox {
 tasks.publishPlugin {
     // dependsOn(testDotNet)
     dependsOn(tasks.buildPlugin)
-    token.set(System.getenv("PublishKey"))
+    token.set(System.getenv("PublishToken"))
 
     doLast {
         exec {
             executable("dotnet")
-            args("nuget","push","output/${DotnetPluginId}.${version}.nupkg","--api-key", System.getenv("PublishKey"),"--source","https://plugins.jetbrains.com")
+            args("nuget","push","output/${DotnetPluginId}.${version}.nupkg","--api-key", System.getenv("PublishToken"),"--source","https://plugins.jetbrains.com")
             workingDir(rootDir)
         }
     }
